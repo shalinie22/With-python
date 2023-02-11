d8f0b095cbd351bea833c59657000c980e6587a3
Shalinie <60962646+shalinie22@users.noreply.github.com>
2023-02-11 21:12:28 +0530

    Create class_instances_variables.py

---
+++ classes and objects/class_instances_variables.py
@@ -0,0 +1,18 @@
+class program:
+    def __init__(self,course,tech):
+        self.course=course
+        self.tech=tech
+    
+obj=program("AI","python")
+obj.course
+obj.tech
+
+obj1=program("ML","R")
+obj1.course
+obj1.tech
+
+
+print(obj.course)
+print(obj.tech)
+print(obj1.course)
+print(obj1.tech)
