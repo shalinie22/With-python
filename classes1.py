03c920c1636cdf88cb3a059f3dcbe2881038b6c9
Shalinie <60962646+shalinie22@users.noreply.github.com>
2023-02-11 20:04:17 +0530

    Add files via upload

---
+++ classes1.py
@@ -0,0 +1,16 @@
+# class basic code
+class Hello:
+    def __init__(self,name):
+        self.name=name
+    def first(self):
+        print("Hello")
+
+hai=Hello("shalinie")
+same=hai.name
+hai.first()
+print(same)
+
+
+
+
+
